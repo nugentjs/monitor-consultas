985b1310bd39887823dd4947490c85626b31b64a
nugentjs <nugentjs@gmail.com>
2026-06-03 01:21:33 +0000

    adiciona coluna agencia na tabela

--- src/components/GanttChart.jsx
+++ src/components/GanttChart.jsx
@@ -1,6 +1,10 @@
 import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 dayjs.extend(customParseFormat)
+dayjs.extend(isSameOrAfter)
+dayjs.extend(isSameOrBefore)
 
 export default function GanttChart({ consultas }) {
   const hoje = dayjs().startOf('day')
